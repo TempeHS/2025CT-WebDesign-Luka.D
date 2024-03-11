8c4032fe0446888d1562fc04c51d1aa9e48553a2
LukaDavies <162514942+LukaDavies@users.noreply.github.com>
2024-03-11 14:03:20 +1100

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ loadDIV ();
 
 
 function loadDIV () {
-    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello" + enterName +"<H1></H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello" + enterName +"</H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "</H2>Guess a number between 1 and 10"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+}
+
+function enterNumber () {
+    playerGuess = prompt("Enter a number")
 }
